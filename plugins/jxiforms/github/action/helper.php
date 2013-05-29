df36d5b926403ad0171948b4c86e74bcc70f17b4
Bhavya Shaktawat <bhavya@readybytes.in>
2013-05-29 13:32:27 +0530

    help message updated in chargify and hubspot action and added curl certificate path(cacert.pem) in curl request, without the certification curl based action will not work on windows since in windows, curl package does not include certificate file by-default

--- plugins/jxiforms/github/action/helper.php
+++ plugins/jxiforms/github/action/helper.php
@@ -29,6 +29,8 @@ class JXiFormsActionGithubHelper extends JXiFormsHelper
 	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
 	    curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
 	    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);   
+	    curl_setopt($ch, CURLOPT_USERAGENT, "team+jxiforms@readybytes.in");
+	    curl_setopt($ch, CURLOPT_CAINFO, JPATH_SITE.'/libraries/joomla/http/transport/cacert.pem');
 	    $response = curl_exec($ch);
 	    
 	    $header_size 		 = curl_getinfo($ch,CURLINFO_HEADER_SIZE);
