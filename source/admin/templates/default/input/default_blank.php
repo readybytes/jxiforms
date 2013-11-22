86a810cb31fd35b65ce113055d0510514edf6bd9
Bhavya Shaktawat <bhavya@readybytes.in>
2013-11-22 16:24:05 +0530

    form-id added and tokens added for package build

--- source/admin/templates/default/input/default_blank.php
+++ source/admin/templates/default/input/default_blank.php
@@ -8,7 +8,7 @@
 */
 if(defined('_JEXEC')===false) die();?>
 
-<form action="<?php echo $uri; ?>" method="post" name="adminForm">
+<form action="<?php echo $uri; ?>" method="post" name="adminForm" id="adminForm">
 	<div class="row-fluid">
 		<div class="span12">
 			<p class="lead center"><?php echo $heading; ?></p>
