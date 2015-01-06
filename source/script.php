a22ee2f90f0517a6e03fe2a86604bad481bdb7ff
Bhavya <bhavya@readybytes.in>
2015-01-06 15:52:00 +0530

    Chore :: Removes rbinstaller existance checking prior to RbFramework upgrade
    Removes exact version checking for rbframework.

--- source/script.php
+++ source/script.php
@@ -206,7 +206,7 @@ class Com_jxiformsInstallerScript
 		$query	= $db->getQuery(true);
      	$query->select('*')
 	     	  ->from($db->quoteName('#__extensions'))
-	     	  ->where('`type` = '.$db->quote('component'). ' AND  `element` LIKE '.$db->quote('com_payinvoice'). ' OR `element` LIKE '.$db->quote('com_rbinstaller'));
+	     	  ->where('`type` = '.$db->quote('component'). ' AND  `element` LIKE '.$db->quote('com_payinvoice'));
 	     	  
      	$db->setQuery($query);
 		$installed_extensions = $db->loadObjectList();
@@ -224,8 +224,7 @@ class Com_jxiformsInstallerScript
 			$installed_rb_version 	=  explode('.', $params['version']);
 			
 			//if there is no change in the major version of rbframework then install else show message
-			if(version_compare($installed_rb_version[0].'.'.$installed_rb_version[1], $latest_rb_version[0].'.'.$latest_rb_version[1]) == 0
-				&& version_compare($content['rbframework']['version'], $params['version']) != 0){
+			if(version_compare($installed_rb_version[0].'.'.$installed_rb_version[1], $latest_rb_version[0].'.'.$latest_rb_version[1]) == 0){
 				$this->installRBFramework($content['rbframework']);
 				if(!$result->enabled){
 					$this->changeExtensionState(array(array('type'=>'system',   'name'=>'rbsl')));
